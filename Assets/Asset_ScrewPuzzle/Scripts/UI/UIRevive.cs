6550153cfa63e98c78a68d1faaf90c140e8cfac9
hongquansp <hongquansp2811@gmail.com>
2024-08-22 17:37:03 +0700

    Update Level

--- Assets/Asset_ScrewPuzzle/Scripts/UI/UIRevive.cs
+++ Assets/Asset_ScrewPuzzle/Scripts/UI/UIRevive.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+//using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class UIRevive : UICanvas
 {
